8ea20500cc404be904d4a5c12a513cb22c6b44c7
EnhasuKim <mj040402@daum.net>
2021-04-17 17:57:02 +0900

    이미지 다운로더

---
+++ test.py
@@ -0,0 +1,11 @@
+[pip install]
+numpy: 수치연산 위한 유용한/효율적인 기능 제공
+pillow: 이미지 파일을 처리하기 위한 유용한 기능 제공
+opencv-python: 영상처리를 위한 유용한 기능 제공
+scipy: 과학 기술 계산을 위한 패키지
+
+face_recognition: 얼굴 인식을 위한 패키지
+
+[conda install]
+cmake : 소스파일 빌드(운영체제별로 동일한 기능을 수행하는 실행파일을 생성하기 위한 도구)
+dlib : 인공지능 기계학습을 위한 기능을 제공하는 패키지
